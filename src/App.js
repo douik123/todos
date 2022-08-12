a2a8b16237805ea413149ac3c457c162c818a2ce
douik123 <ahmed@predict-a.com>
2022-08-12 18:58:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { v4 as uuidv4} from 'uuid'
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { AiFillEdit } from "react-icons/ai";
 
 function App() {
+  const [tab, setTab] = useState([{task:"Ahmed",id:uuidv4()},{task:"Ksia",id:uuidv4()},{task:"Houcem",id:uuidv4()},{task:"Mariem",id:uuidv4()}]);
+  const [input, setInput] = useState("");
+  const [showEdit,setShowEdit] = useState(false);
+  const addNewTab=(e)=>{
+    setTab([...tab,{task:input,id:uuidv4()}]);
+  }
+  function deleteTodo(id) {
+    let updatedTodos = tab.filter((el) => el.id !== id);
+    setTab(updatedTodos);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">Todo App</div>
+      <br></br>
+      <div>
+       <div className="common"><input type="text"  placeholder="Enter task" onChange={(e)=>setInput(e.target.value)}/><div><button className="add" onClick={addNewTab} >ADD</button></div></div> 
+       <div className="tableau">
+       <br></br>
+       <div className="todos">
+       {showEdit ? <h1>Hello!</h1> :<> {tab.map(el=>
+        <div className="tabs">
+          <div><h3>{el.task}</h3></div>
+          <div><button className="delete" onClick={() => deleteTodo(el.id)} ><RiDeleteBin6Line/></button>
+          <button className="edit" onClick={()=>setShowEdit(true)}><AiFillEdit/></button>
+        </div>
+        </div>
+        
+          )}</>}
+          </div>
+      
+      </div>
+
+
+
+
+     </div>
     </div>
   );
 }
-
 export default App;
