5ba73def928e4a0ebfbcaa45e9f9134f2a3f1d15
douik123 <ahmed@predict-a.com>
2022-08-13 23:17:19 +0200

    projet todos done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 import { v4 as uuidv4} from 'uuid'
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { AiFillEdit } from "react-icons/ai";
-
+import { MdOutlineDone } from "react-icons/md";
 function App() {
   const [tab, setTab] = useState([{task:"Ahmed",id:uuidv4()},{task:"Ksia",id:uuidv4()},{task:"Houcem",id:uuidv4()},{task:"Mariem",id:uuidv4()}]);
   const [input, setInput] = useState("");
-  const [showEdit,setShowEdit] = useState(false);
+  // const [showEdit,setShowEdit] = useState(false);
+  const [todoediting, setTodoediting] = useState(null);
+  const [editingtext, seteditingtext] = useState("");
+
   const addNewTab=(e)=>{
     setTab([...tab,{task:input,id:uuidv4()}]);
   }
@@ -15,7 +18,16 @@ function App() {
     let updatedTodos = tab.filter((el) => el.id !== id);
     setTab(updatedTodos);
   }
-
+  function submitEdits(id) {
+    const updatedTodos = [...tab].map((todo) => {
+      if (todo.id === id) {
+        todo.task = editingtext;
+      }
+      return todo;
+    });
+    setTab(updatedTodos);
+    setTodoediting(null);
+  }
 
   return (
     <div className="container">
@@ -26,7 +38,7 @@ function App() {
        <div className="tableau">
        <br></br>
        <div className="todos">
-       {showEdit ? <h1>Hello!</h1> :<> {tab.map(el=>
+       {/* {showEdit ? <h1>Hello!</h1> :<> {tab.map(el=>
         <div className="tabs">
           <div><h3>{el.task}</h3></div>
           <div><button className="delete" onClick={() => deleteTodo(el.id)} ><RiDeleteBin6Line/></button>
@@ -34,7 +46,35 @@ function App() {
         </div>
         </div>
         
-          )}</>}
+          )}</>} */}
+          {tab.map(el=>
+        <div className="tabs">
+          {todoediting===el.id ? 
+          (<input
+          type="text"
+          onChange={(e)=>seteditingtext(e.target.value)}
+          />) 
+          :
+          (<div><h3>{el.task}</h3></div>)}
+          
+          <div className="buttons">
+          {el.id === todoediting ? (
+            <button className="edit" onClick={() => submitEdits(el.id)}><MdOutlineDone/></button>
+            ) : (
+              <div>
+              <button className="edit" onClick={()=>setTodoediting(el.id)}><AiFillEdit/></button>
+              <button className="delete" onClick={() => deleteTodo(el.id)} ><RiDeleteBin6Line/></button>
+              </div>
+            )}
+          
+          
+          
+          
+          
+        </div>
+        </div>
+        
+          )}
           </div>
       
       </div>
